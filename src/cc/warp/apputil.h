76f82b29ddf6038033339c9468261fe8fe4b61e4
Josh Taylor <jtaylor@cs.stanford.edu>
2008-07-07 16:21:20 -0700

    Rewrote lots of file header comments.
    All files used to be part of "KDI".  Now common library files are part of their respective libraries.

--- src/cc/warp/apputil.h
+++ src/cc/warp/apputil.h
@@ -1,23 +1,22 @@
 //---------------------------------------------------------- -*- Mode: C++ -*-
 // Copyright (C) 2006 Josh Taylor (Kosmix Corporation)
 // Created 2006-01-10
-//
-// This file is part of KDI.
 // 
-// KDI is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// any later version.
+// This file is part of the warp library.
 // 
-// KDI is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-// License for more details.
+// The warp library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or any later
+// version.
 // 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301, USA.
+// The warp library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 //----------------------------------------------------------------------------
 
 #ifndef WARP_APPUTIL_H
