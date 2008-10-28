77b9f80bd0504e886e72f47fa60bb07d46540011
Josh Taylor <jtaylor@cs.stanford.edu>
2008-10-28 14:47:33 -0700

    Fixed file headers with OSS license

--- src/cc/warp/base64_test.cc
+++ src/cc/warp/base64_test.cc
@@ -1,12 +1,22 @@
 //---------------------------------------------------------- -*- Mode: C++ -*-
-// $Id: warp/base64_test.cc $
-//
-// Created 2008/08/28
-//
-// Copyright 2008 Kosmix Corporation.  All rights reserved.
-// Kosmix PROPRIETARY and CONFIDENTIAL.
-//
+// Copyright (C) 2008 Josh Taylor (Kosmix Corporation)
+// Created 2008-08-28
 // 
+// This file is part of the warp library.
+// 
+// The warp library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or any later
+// version.
+// 
+// The warp library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 //----------------------------------------------------------------------------
 
 #include <warp/base64.h>
