6a166fc7b652484c4369ff0ca05c2fd87702069e
Josh Taylor <jtaylor@cs.stanford.edu>
2009-01-28 17:53:42 -0800

    Made each scan test be less verbose and print aggregate stats

--- src/cc/kdi/app/scan_test.cc
+++ src/cc/kdi/app/scan_test.cc
@@ -214,6 +214,9 @@ void runScanners(size_t id,
                  UniqueStringSampler * columnSampler,
                  UniqueStringSampler * familySampler)
 {
+    log("Scan thread %d starting: %d scan(s)", id, nScans);
+
+    AddingVisitor v;
     for(size_t i = 0; i < nScans; ++i)
     {
         ScanPredicate p(pred);
@@ -243,15 +246,23 @@ void runScanners(size_t id,
                 chooseSample(familySampler->getSamples(), true));
         }
         
-        log("Scan %d.%d starting: pred=(%s)", id, i, p);
+        if(verbose)
+            log("Scan %d.%d running: pred=(%s)", id, i, p);
 
-        AddingVisitor v;
+        size_t nCells = v.nCells;
+        size_t nBytes = v.nBytes;
+
         doScan(v, tables, p);
 
-        log("Scan %d.%d done: %s cells, %s bytes", id, i,
-            sizeString(v.nCells, 1000),
-            sizeString(v.nBytes, 1024));
+        if(verbose)
+            log("Scan %d.%d done: %s cells, %s bytes", id, i,
+                sizeString(v.nCells - nCells, 1000),
+                sizeString(v.nBytes - nBytes, 1024));
     }
+
+    log("Scan thread %d done: %s cells, %s bytes", id,
+        sizeString(v.nCells, 1000),
+        sizeString(v.nBytes, 1024));
 }
 
 
