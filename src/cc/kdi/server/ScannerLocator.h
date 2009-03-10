34ff2823d4ae8d2b7caab7ef0eb82b0eead00d89
Josh Taylor <jtaylor@cs.stanford.edu>
2009-03-10 16:00:58 -0700

    Header fixup

--- src/cc/kdi/server/ScannerLocator.h
+++ src/cc/kdi/server/ScannerLocator.h
@@ -1,12 +1,21 @@
 //---------------------------------------------------------- -*- Mode: C++ -*-
-// $Id: kdi/server/ScannerLocator.h $
+// Copyright (C) 2009 Josh Taylor (Kosmix Corporation)
+// Created 2009-02-27
 //
-// Created 2009/02/27
+// This file is part of KDI.
 //
-// Copyright 2009 Kosmix Corporation.  All rights reserved.
-// Kosmix PROPRIETARY and CONFIDENTIAL.
+// KDI is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or any later version.
 //
-// 
+// KDI is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 //----------------------------------------------------------------------------
 
 #ifndef KDI_SERVER_SCANNERLOCATOR_H
