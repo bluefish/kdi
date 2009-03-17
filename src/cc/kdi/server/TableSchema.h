52186dc9ba58e4cb2c94ab6090a5f94a02d831cb
Josh Taylor <jtaylor@cs.stanford.edu>
2009-03-17 09:35:58 -0700

    Added TableSchema and TabletConfig

---
+++ src/cc/kdi/server/TableSchema.h
@@ -0,0 +1,50 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// Copyright (C) 2009 Josh Taylor (Kosmix Corporation)
+// Created 2009-03-13
+//
+// This file is part of KDI.
+//
+// KDI is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or any later version.
+//
+// KDI is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//----------------------------------------------------------------------------
+
+#ifndef KDI_SERVER_TABLESCHEMA_H
+#define KDI_SERVER_TABLESCHEMA_H
+
+#include <string>
+#include <vector>
+
+namespace kdi {
+namespace server {
+
+    struct TableSchema
+    {
+        struct Group
+        {
+            std::string name;
+            std::string compressor;
+            std::vector<std::string> columns; // empty() means "include all"
+            int64_t maxAge;                   // <= 0 means "keep all"
+            int64_t maxHistory;               // <= 0 means "keep all"
+            size_t diskBlockSize;
+            bool inMemory;
+        };
+
+        std::string tableName;
+        std::vector<Group> groups;
+    };
+
+} // namespace server
+} // namespace kdi
+
+#endif // KDI_SERVER_TABLESCHEMA_H
---
+++ src/cc/kdi/server/TabletConfig.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// Copyright (C) 2009 Josh Taylor (Kosmix Corporation)
+// Created 2009-03-13
+//
+// This file is part of KDI.
+//
+// KDI is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or any later version.
+//
+// KDI is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//----------------------------------------------------------------------------
+
+#ifndef KDI_SERVER_TABLETCONFIG_H
+#define KDI_SERVER_TABLETCONFIG_H
+
+#include <warp/interval.h>
+#include <vector>
+#include <string>
+
+namespace kdi {
+namespace server {
+
+    struct TabletConfig
+    {
+        std::string tableName;
+        warp::Interval<std::string> rows;
+        std::vector<std::string> fragments;
+        std::string log;
+        std::string location;
+
+        std::string getTabletName() const;
+    };
+
+} // namespace server
+} // namespace kdi
+
+#endif // KDI_SERVER_TABLETCONFIG_H
